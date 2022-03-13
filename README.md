e4bfd2f16e1e68fdf586ee9b2fb322cd8147c702
MrMicky <git@mrmicky.fr>
2022-03-13 19:42:12 +0100

    Version 1.0.0 for Azuriom v1.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Carbon (Theme)
 
 [![Style](https://github.styleci.io/repos/237529108/shield)](https://github.styleci.io/repos/237529108)
-[![Chat](https://img.shields.io/discord/625774284823986183?color=7289da&label=Discord&logo=discord&logoColor=fff&style=flat-square)](https://azuriom.com/discord)
+[![Chat](https://img.shields.io/discord/625774284823986183?color=5865f2&label=Discord&logo=discord&logoColor=fff&style=flat-square)](https://azuriom.com/discord)
 
 A light and dark Carbon theme.
--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -1,71 +1,9 @@
-<!DOCTYPE html>
-@include('elements.base')
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="@yield('description', setting('description', ''))">
-    <meta name="theme-color" content="#3490DC">
-    <meta name="author" content="Azuriom">
+@extends('layouts.base')
 
-    <meta property="og:title" content="@yield('title')">
-    <meta property="og:type" content="@yield('type', 'website')">
-    <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:image" content="{{ favicon() }}">
-    <meta property="og:description" content="@yield('description', setting('description', ''))">
-    <meta property="og:site_name" content="{{ site_name() }}">
-    @stack('meta')
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('title') | {{ site_name() }}</title>
-
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="{{ favicon() }}">
-
-    <!-- Scripts -->
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}" defer></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}" defer></script>
-    <script src="{{ asset('vendor/axios/axios.min.js') }}" defer></script>
-    <script src="{{ asset('js/script.js') }}" defer></script>
-
-    <!-- Page level scripts -->
-    @stack('scripts')
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
-    <link href="{{ asset('vendor/fontawesome/css/all.min.css') }}" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ theme_asset('css/style.css') }}" rel="stylesheet">
-    @stack('styles')
-</head>
-
-<body>
-<div id="app">
-    @include('elements.navbar')
-
-    <main>
-        <div class="container">
-            @include('elements.session-alerts')
-        </div>
+@section('app')
+    <main class="container content">
+        @include('elements.session-alerts')
 
         @yield('content')
     </main>
-</div>
-
-<footer class="text-white py-4 text-center">
-    <div class="copyright">
-        <div class="container">
-            {{ setting('copyright') }} | @lang('messages.copyright')
-        </div>
-    </div>
-</footer>
-
-@stack('footer-scripts')
-
-</body>
-</html>
+@endsection
