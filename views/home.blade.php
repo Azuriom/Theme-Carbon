f313fd833143d10e334100712a7e73bcd462f955
MrMicky <git@mrmicky.fr>
2022-07-31 11:14:21 +0200

    Add servers in homepage

--- views/home.blade.php
+++ views/home.blade.php
@@ -30,6 +30,53 @@
             </div>
         @endif
 
+        @if(! $servers->isEmpty())
+            <h2 class="text-center">
+                {{ trans('messages.servers') }}
+            </h2>
+
+            <div class="row gy-3 justify-content-center mb-5">
+                @foreach($servers as $server)
+                    <div class="col-md-4">
+                        <div class="card h-100">
+                            <div class="card-body text-center">
+                                <h3 class="card-title">
+                                    {{ $server->name }}
+                                </h3>
+
+                                @if($server->isOnline())
+                                    <div class="progress mb-1">
+                                        <div class="progress-bar" role="progressbar" style="width: {{ $server->getPlayersPercents() }}%">
+                                        </div>
+                                    </div>
+
+                                    <p class="mb-1">
+                                        {{ trans_choice('messages.server.total', $server->getOnlinePlayers(), [
+                                            'max' => $server->getMaxPlayers(),
+                                        ]) }}
+                                    </p>
+                                @else
+                                    <p>
+                                        <span class="badge bg-danger text-white">
+                                            {{ trans('messages.server.offline') }}
+                                        </span>
+                                    </p>
+                                @endif
+
+                                @if($server->joinUrl())
+                                    <a href="{{ $server->joinUrl() }}" class="btn btn-primary">
+                                        {{ trans('messages.server.join') }}
+                                    </a>
+                                @else
+                                    <p class="card-text">{{ $server->fullAddress() }}</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+
         <div class="row">
             <div class="col-md-8">
                 @foreach($posts as $post)
